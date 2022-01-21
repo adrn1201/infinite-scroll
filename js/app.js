258a1fbb1e27a2935a581c36e890b3f09c82465a
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2022-01-21 17:22:18 +0800

    Finalized Code

--- js/app.js
+++ js/app.js
@@ -1,67 +1,96 @@
-const allQuotes = document.querySelector('.quotes');
-const loader = document.querySelector('.loader');
+(function () {
 
-const getQuotes = async(page, limit) => {
-    try{
-        const data = await axios.get(`https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`);
-        return data;
-    } catch(e){
-        console.log(e);
+    const quotesEl = document.querySelector('.quotes');
+    const loaderEl = document.querySelector('.loader');
+
+    const getQuotes = async (page, limit) => {
+        const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`;
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+            throw new Error(`An error occurred: ${response.status}`);
+        }
+        return await response.json();
     }
-}
 
-const showQuotes = (quotes) => {
-    for(const quote of quotes){
-        const newQuoteEl = document.createElement('blockquote');
-        newQuoteEl.classList.add('quote');
-        newQuoteEl.innerHTML = `
+    const showQuotes = (quotes) => {
+        quotes.forEach(quote => {
+            const quoteEl = document.createElement('blockquote');
+            quoteEl.classList.add('quote');
+
+            quoteEl.innerHTML = `
             <span>${quote.id})</span>
             ${quote.quote}
             <footer>${quote.author}</footer>
         `;
-        allQuotes.append(newQuoteEl);
-    }
-};
 
-const hideLoader = () => {
-    loader.classList.remove('show')
-}
+            quotesEl.appendChild(quoteEl);
+        });
+    };
 
-const showLoader = () => {
-    loader.classList.add('show');
-}
+    const hideLoader = () => {
+        loaderEl.classList.remove('show');
+    };
 
-let currentPage = 1;
-const limit = 10;
-let total = 0;
+    const showLoader = () => {
+        loaderEl.classList.add('show');
+    };
 
-const hasMoreQuotes = (page, limit, total) => {
-    const startIndex = (page - 1) * limit + 1;
-    return total === 0 || startIndex < total;
-};
+    const hasMoreQuotes = (page, limit, total) => {
+        const startIndex = (page - 1) * limit + 1;
+        return total === 0 || startIndex < total;
+    };
 
-const loadQuotes = async (page, limit) => {
+    // load quotes
+    const loadQuotes = async (page, limit) => {
 
-    // show the loader
-    showLoader();
+        // show the loader
+        showLoader();
 
-    // 0.5 second later
-    setTimeout(async () => {
-        try {
-            // if having more quotes to fetch
-            if (hasMoreQuotes(page, limit, total)) {
-                // call the API to get quotes
-                const response = await getQuotes(page, limit);
-                // show quotes
-                showQuotes(response.data);
-                // update the total
-                total = response.total;
+        // 0.5 second later
+        setTimeout(async () => {
+            try {
+                // if having more quotes to fetch
+                if (hasMoreQuotes(page, limit, total)) {
+                    // call the API to get quotes
+                    const response = await getQuotes(page, limit);
+                    // show quotes
+                    showQuotes(response.data);
+                    // update the total
+                    total = response.total;
+                }
+            } catch (error) {
+                console.log(error.message);
+            } finally {
+                hideLoader();
             }
-        } catch (error) {
-            console.log(error.message);
-        } finally {
-            hideLoader();
-        }
-    }, 500);
+        }, 500);
 
-};
+    };
+
+    
+    let currentPage = 1;
+    const limit = 10;
+    let total = 0;
+
+
+    window.addEventListener('scroll', () => {
+        const {
+            scrollTop,
+            scrollHeight,
+            clientHeight
+        } = document.documentElement;
+
+        if (scrollTop + clientHeight >= scrollHeight - 5 &&
+            hasMoreQuotes(currentPage, limit, total)) {
+            currentPage++;
+            loadQuotes(currentPage, limit);
+        }
+    }, {
+        passive: true
+    });
+
+    
+    loadQuotes(currentPage, limit);
+
+})();
